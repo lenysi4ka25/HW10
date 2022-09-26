6d4ebfb20c44f6a6a2bf3c7af941baa0d12c8087
lenysi4ka25 <lenysi4ka@mail.ru>
2022-09-26 20:53:14 +0300

    Возникли трудности. Спасибо за подробный разбор домашки)))

--- src/Main.java
+++ src/Main.java
@@ -47,13 +47,13 @@ public class Main {
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
-        }
+                    }
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_00) + 100_000;
-        }
+            }
         return arr;
     }}
